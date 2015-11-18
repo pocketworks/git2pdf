5b4ac0939492e096e6f2cddf3ee489f0796e56db
jgrau <jonas.grau@gmail.com>
2015-11-18 13:29:28 +0100

    Use userstory as feature tag

--- lib/git2pdf.rb
+++ lib/git2pdf.rb
@@ -48,7 +48,7 @@ class Git2Pdf
 
         type = ""
         type = "BUG" if labels =~ /bug/i #not billable
-        type = "FEATURE" if labels =~ /feature/i #billable
+        type = "FEATURE" if labels =~ /userstory/i #billable
         type = "ENHANCEMENT" if labels =~ /enhancement/i #billable
         type = "AMEND" if labels =~ /amend/i #not billable
         type = "TASK" if labels =~ /task/i #not billable
